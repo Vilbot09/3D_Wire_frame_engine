fb0598c5d491755ffeafd40451fc38a42d42092f
Hjalram <jolshjalram@gmail.com>
2025-04-15 16:33:35 +0200

    basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
+#include "raylib/raylib.h"
 
-int main(void) {
+int main(void)
+{
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
